76e4b4102f61791e0637c61a98d6d22a1c7f9639
Robin Cominotto <robin.cominotto@ap.be>
2026-06-26 18:50:59 +0200

    Workflow added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/github-actions-react-app/',
   test: {
     globals: true,
     environment: 'jsdom',
